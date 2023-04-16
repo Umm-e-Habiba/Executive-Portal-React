e144b6c31b14015a7bb95a5ac1ce3b3c7689ff78
Umm E Habiba <ummehabiba989@gmail.com>
2023-04-16 13:44:34 +0500

    v2

--- src/components/PageTitle.js
+++ src/components/PageTitle.js
@@ -13,7 +13,7 @@ const PageTitle = (props) => {
             
             <Breadcrumb className="float-right mt-1 font-size-14">
                 <BreadcrumbItem>
-                    <Link to="/">Attari Admin</Link>
+                    <Link to="/">Attarii Admin</Link>
                 </BreadcrumbItem>
                 {items.map((item, index) => {
                     return item.active ? (
